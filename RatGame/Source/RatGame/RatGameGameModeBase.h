70ae9fdd6b269c4fcc939fe7512eeb5fd913b16f
RAT <rapid.automatic.technology@gmail.com>
2018-02-27 09:36:38 +0900

    ソース追加

---
+++ RatGame/Source/RatGame/RatGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RatGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RATGAME_API ARatGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
